e2386636275bb597e4661f4081e0a0cba8fc022c
Emerson Leite <emerson.bl@gmail.com>
2020-05-28 09:21:02 -0300

    docs

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 /* Configuração de domínio no github pages */
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/spend-bill/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/spend-bills-money/" : "/",
   outputDir: "docs",
 };
